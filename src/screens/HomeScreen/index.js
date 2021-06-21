a7c1750a4a27e6c008485034c8343c8994fb9e0a
Hiren Vaghela <vaghelahiren.ss@smartdatainc.io>
2021-06-21 22:10:32 +0530

    -Resolve TVOS not run issue fixed, - Implement Firebase crashalytics -InProgress, - Cursor movement detection and animation - InProgress

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -8,68 +8,144 @@ import {
   TouchableHighlight,
   Modal,
   Image,
-  Dimensions
+  Dimensions,
+  Pressable
   } from 'react-native';
   import { connect } from 'react-redux';
   import {INCREASE_COUNTER, DECREASE_COUNTER} from 'src/redux/const'
 import { SET_COUNTER } from '../../redux/const';
 import colors from 'src/helper/colors'
+//import firebase from '../../helper/firebase';
+// import crashlytics from '@react-native-firebase/crashlytics';
 const {width} = Dimensions.get('window')
+const [MY_LIST, MINUS, PLUS, RESET] = [0, 1, 2, 3]
 class HomeScreen extends React.Component {
 state = {
   posts: [],
   modalVisible:false,
+  cursorState:0
 };
 
-componentDidMount() {
+   _tvEventHandler=undefined
+
+  _enableTVEventHandler() {
+    console.log("_enableTVEventHandler...1")
+    this._tvEventHandler = new TVEventHandler();
+    console.log("_enableTVEventHandler...2")
+    this._tvEventHandler.enable(this, function(cmp, evt) {
+      console.log("_enableTVEventHandler...3", evt);
+      if (evt && evt.eventType === 'right') {
+        console.log("RIGHT")
+      } else if(evt && evt.eventType === 'up') {
+        console.log("UP")
+      } else if(evt && evt.eventType === 'left') {
+        console.log("LEFT")
+      } else if(evt && evt.eventType === 'down') {
+        console.log("DOWN")
+      }
+    });
+  }
+
+  _disableTVEventHandler() {
+    if (this._tvEventHandler) {
+      this._tvEventHandler.disable();
+      delete this._tvEventHandler;
+    }
+  }
+
+  componentWillUnmount() {
+    this._disableTVEventHandler();
+  }
+async componentDidMount() {
   fetch('https://www.reddit.com/r/pic/top.json?t=year')
     .then(response => response.json())
     .then(json => {
       const posts = json.data.children.map(child => child.data);
       this.setState({posts});
     });
+    console.log("componentDidMount...1")
+    this._enableTVEventHandler();
+    console.log("componentDidMount...2")
+    //crashlytics().log('crashlytics log.');
+   
+    
 }
 
 render() {
+  const {cursorState} = this.state
+  console.log({cursorState, MY_LIST: cursorState == MY_LIST, MINUS: cursorState == MINUS, PLUS:cursorState == MY_LIST, RESET:cursorState == RESET})
   return (<>
-  <View style={{flexDirection:'row'}}>
-  <TouchableHighlight onPress={()=> this.props.navigation.navigate("MyListScreen")}>
-    <Text>HomeScreen </Text>
-  </TouchableHighlight>
+    
+    <View style={{flexDirection:'row', margin:20}}>
 
-  <TouchableHighlight onPress={()=> this.props.reduxDecreaseCounter()} >
-      <Text> - </Text>
-  </TouchableHighlight>
+      <Pressable
+        tvParallaxProperties={{ magnification: 1.1 }}
+        style={({pressed, focused}) => 
+          ({
+            backgroundColor: focused? 'orange': pressed ? 'red' : 'transparent',
+          })
+        }
 
-    <Text>{this.props.counter}</Text>
+        onPress={()=> this.props.navigation.navigate("MyListScreen")}>
+        <Text style={{fontSize:24}}>My List </Text>    
+      </Pressable>
 
-  <TouchableHighlight onPress={()=> this.props.reduxIncreaseCounter()} >
-      <Text> + </Text>
-  </TouchableHighlight>
+      <Pressable
+        tvParallaxProperties={{ magnification: 1.1 }}
+        style={({pressed, focused}) => 
+          ({
+            backgroundColor: focused? 'orange': pressed ? 'red' : 'transparent',
+            overflow:'hidden'
+          })
+        }
 
-  <TouchableHighlight onPress={()=> {
-    this.props.reduxSetCounter(0)
-    }} >
-      <Text> Reset </Text>
-  </TouchableHighlight>
+        onPress={()=> this.props.reduxDecreaseCounter()}>
+        <Text style={{fontSize:24}}> - </Text>    
+      </Pressable>
+    
+      <Text style={{fontSize:24}}>{this.props.counter}</Text>
+
+      <Pressable
+        tvParallaxProperties={{ magnification: 1.1 }}
+        style={({pressed, focused}) => 
+          ({
+            backgroundColor: focused? 'orange': pressed ? 'red' : 'transparent',
+          })
+        }
+
+        onPress={()=> this.props.reduxIncreaseCounter()}>
+        <Text style={{fontSize:24}}> + </Text>    
+      </Pressable>
+    
+      <Pressable
+        tvParallaxProperties={{ magnification: 1.1 }}
+        style={({pressed, focused}) => 
+          ({
+            backgroundColor: focused? 'orange': pressed ? 'red' : 'transparent',
+          })
+        }
+
+        onPress={()=> this.props.reduxSetCounter(0)}>
+        <Text style={{fontSize:24}}> reset </Text>    
+      </Pressable>
+    
 
 
-  </View>
+
+    </View>
   <ScrollView contentContainerStyle={styles.container}>
 {this.state.posts.map(post => (
   <View style={styles.tile} key={post.id}>
-    <TouchableHighlight
-      style={styles.highlight}
-      underlayColor='#a8dadc'
-      // We use onPress to open Modal and to set selected image url to state
-      onPress={() => this.setState({ modalVisible: true, selectedImage: post.url })}
+    <Pressable
+        tvParallaxProperties={{ magnification: 1.001 }}
+        style={({pressed, focused}) => focused ? styles.highlightFocused : styles.highlight}
+        onPress={() => this.setState({ modalVisible: true, selectedImage: post.url })}
     >
       <ImageBackground
         style={{ width: '100%', height: '100%' }}
         source={{ uri: post.thumbnail }}
-        imageStyle={styles.background}
       />
-    </TouchableHighlight>
+    </Pressable>
     <Text style={styles.title}>{post.title}</Text>
   </View>
 ))}
@@ -141,12 +217,19 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     padding: 10,
   },
-  background: {
+  highlight:{
     borderColor: '#1d3557',
+    borderRadius: 20,
+    borderColor: 'green',
+  },
+  highlightFocused:{
+    borderWidth:5,
+    borderColor: 'orange',
     borderRadius: 20,
   },
   title: {
     fontSize: 20,
     textAlign: 'center',
   },
+  
   });
